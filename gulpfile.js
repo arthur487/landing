abe437cb3567e04f2081c661daa5760436495086
arthur <arthurdomiciano46@gmail.com>
2025-07-06 09:48:33 -0400

    finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const images = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/*.scss')
     .pipe(sass({outputStyle: 'compressed' }))
-    .pipe(gulp.dest('.dist/css'))
+    .pipe(gulp.dest('.dist/css'));
 }
-exports.default = styles
+
+function Imagemin() {
+    return gulp.src('./src/images/*')
+    .pipe(images())
+    .pipe(gulp.dest('.dist/images'));
+    
+}
+
+exports.default = gulp.parallel(styles, Imagemin);
+
 exports.watch = function() {
-    gulp.watch('./src/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/*.scss', gulp.parallel(styles));
 }
